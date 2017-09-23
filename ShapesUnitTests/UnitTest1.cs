56c7426ffe519457cc7fe6c4a325d210f4e9c732
BaranovMykola <mapsg32@gmail.com>
2017-09-23 15:06:57 +0300

    Added unit test project

---
+++ ShapesUnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CSharpInformSystem.Shape;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShapesUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var m = new Moq.Mock<IShape>();
+            //m.SetupProperty(s => s.ComputeSquare(), 228);
+
+            IShape i = new Circle(0,new Point(0,0));
+
+            Assert.AreEqual(i.ComputeSquare(), 0);
+
+        }
+    }
+}
