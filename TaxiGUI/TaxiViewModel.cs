71ebb255f79ff26ba89942d85553f8c3f0298523
BaranovMykola <mapsg32@gmail.com>
2017-10-15 12:29:25 +0300

    Added MVVM pattern for Taxi struct

---
+++ TaxiGUI/TaxiViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TaxiGUI.Annotations;
+
+namespace TaxiGUI
+{
+    class TaxiViewModel : INotifyPropertyChanged
+    {
+        private TaxiParkModel taxiPark;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
