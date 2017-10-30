2bac59b869db598a491e83355d7240c242882722
BaranovMykola <mapsg32@gmail.com>
2017-10-30 19:53:44 +0200

    Added WPF_Shapes project. Implemented MVVM pattern. Added opportunity to draw rectangles.

---
+++ WPF_Shapes/PolygonWrapper.cs
@@ -0,0 +1,11 @@
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WPF_Shapes
+{
+    public class PolygonWrapper
+    {
+        public Polygon Pol { get; set; }
+        public Brush Fill { get; set; }
+    }
+}
---
+++ WPF_Shapes/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿namespace WPF_Shapes
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Shapes;
+    using WPF_Shapes.Annotations;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private readonly List<Point> polyognEdges = new List<Point>();
+
+        public ViewModel()
+        {
+            ClickCanvasCommand = new RelayCommand(ClickCanvas);
+            DrawPolyCommand = new RelayCommand(DrawPoly);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<PolygonWrapper> Polygons { get; set; } = new ObservableCollection<PolygonWrapper>();
+
+        public ICommand ClickCanvasCommand { get; set; }
+
+        public ICommand DrawPolyCommand { get; set; }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ClickCanvas(object parameter)
+        {
+            polyognEdges.Add(Mouse.GetPosition(parameter as IInputElement));
+        }
+
+        private void DrawPoly(object parameter)
+        {
+            var p = new Polygon() { Points = new PointCollection(polyognEdges) };
+            Point center = polyognEdges.Aggregate((a, b) => new Point(a.X + b.X, a.Y + b.Y));
+            center.X /= polyognEdges.Count;
+            center.Y /= polyognEdges.Count;
+            Polygons.Add(new PolygonWrapper() { Pol = p, Fill = Brushes.Black });
+            OnPropertyChanged(nameof(Polygons));
+            
+            polyognEdges.Clear();
+        }
+    }
+}
